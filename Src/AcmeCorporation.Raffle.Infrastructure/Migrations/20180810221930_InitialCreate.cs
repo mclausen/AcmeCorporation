8170057eecf0958197d60cf2a46c4f8d38756c83
Martin Humlund Clausen <martinh.c@me.com>
2018-08-11 00:36:08 +0200

    - Added initial migration

---
+++ Src/AcmeCorporation.Raffle.Infrastructure/Migrations/20180810221930_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeCorporation.Raffle.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SerialNumbers",
+                columns: table => new
+                {
+                    Serial = table.Column<string>(nullable: false),
+                    DateCreatedUtc = table.Column<DateTime>(nullable: false),
+                    UsageCount = table.Column<int>(nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SerialNumbers", x => x.Serial);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RaffleSubmissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    SerialNumberSerial = table.Column<string>(nullable: true),
+                    SubmissionTimeUtc = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RaffleSubmissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RaffleSubmissions_SerialNumbers_SerialNumberSerial",
+                        column: x => x.SerialNumberSerial,
+                        principalTable: "SerialNumbers",
+                        principalColumn: "Serial",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RaffleSubmissions_SerialNumberSerial",
+                table: "RaffleSubmissions",
+                column: "SerialNumberSerial");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RaffleSubmissions");
+
+            migrationBuilder.DropTable(
+                name: "SerialNumbers");
+        }
+    }
+}
