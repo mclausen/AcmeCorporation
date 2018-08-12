81abe58862b7c7e7b30bdf215c3984b2450667c1
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 13:58:25 +0200

    - Added method for containing businesslogic regarding UsageCount - Added Static factory method

--- Src/AcmeCorporation.Raffle.Domain/SerialNumber.cs
+++ Src/AcmeCorporation.Raffle.Domain/SerialNumber.cs
@@ -3,9 +3,29 @@
 namespace AcmeCorporation.Raffle.Domain
 {
     public class SerialNumber
-    {
-        public string Serial { get; set; }
-        public DateTime DateCreatedUtc { get; set; }
-        public int UsageCount { get; set; }
+    {   
+        public string Serial { get; protected set; }
+        public DateTime DateCreatedUtc { get; protected set; }
+        public int UsageCount { get; protected set; }
+
+        public void Use()
+        {
+            if (UsageCount == 2)
+            {
+                throw new DomainException($"Serial number '{Serial}' exeeded its usage count");
+            }
+
+            UsageCount++;
+        }
+
+        public static SerialNumber CreateNewSerialNumber(string serialNumber)
+        {
+            return new SerialNumber
+            {
+                DateCreatedUtc = DateTime.Now,
+                Serial = serialNumber,
+                UsageCount = 0
+            };
+        }
     }
 }
