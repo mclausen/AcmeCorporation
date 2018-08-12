06a642e7861a5d75d6c1067e8a214b840ba52e5e
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 21:45:51 +0200

    Added draw overview component

---
+++ Src/AcmeCorporation-Client/src/app/draw-overview/drawoverview.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed, async } from '@angular/core/testing';
+import { DrawOverviewComponent } from './drawoverview.component';
+
+describe('DrawoOverviewComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DrawOverviewComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the and instance', async(() => {
+    const fixture = TestBed.createComponent(DrawOverviewComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
