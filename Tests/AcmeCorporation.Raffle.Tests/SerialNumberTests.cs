b203e10cb93795c8071be2271552434053fd944f
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 14:16:50 +0200

    - Added SerialNumberTests to validate business rules

---
+++ Tests/AcmeCorporation.Raffle.Tests/SerialNumberTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AcmeCorporation.Raffle.Domain;
+using NUnit.Framework;
+
+namespace AcmeCorporation.Raffle.Tests
+{
+    [TestFixture]
+    public class SerialNumberTests
+    {
+        private SerialNumber sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = SerialNumber.CreateNewSerialNumber(Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        public void Use_UsageCount_ShoulbeOne()
+        {
+            sut.Use();
+            
+            Assert.That(sut.UsageCount, Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void Use_UsageCount_ShoulbeTwo()
+        {
+            sut.Use();
+            sut.Use();
+            
+            Assert.That(sut.UsageCount, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Use_ThirdTime_Throws()
+        {
+            sut.Use();
+            sut.Use();
+            
+            Assert.Throws<DomainException>(() => sut.Use());
+        }
+    }
+}
