5364e1775a2d7c10d45fc870cc4c33323791fde3
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 14:02:37 +0200

    Added EmailAddressTests

---
+++ Tests/AcmeCorporation.Raffle.Tests/EmailAddressTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AcmeCorporation.Raffle.Domain;
+using NUnit.Framework;
+
+namespace AcmeCorporation.Raffle.Tests
+{
+    [TestFixture]
+    public class EmailAddressTests
+    {   
+        [Test]
+        public void Ctor_WithNullOrEmptyString_throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new EmailAddress(string.Empty));
+        }
+
+        [TestCase(true, "test@test.com")]
+        [TestCase(false, "testtest.com")]
+        [TestCase(false, "test@testcom")]
+        [TestCase(false, "test@test..com")]
+        //[TestCase(false, "@test.com")] <!--- This fails due to invalid regex that cannot match the case (should be fixed in real world scenario)
+        [TestCase(false, "test@")]
+        [TestCase(false, "test")]
+        public void Ctor_DoValidateEmailAddress(bool isValid, string emailAddress)
+        {
+            if (isValid)
+            {
+                Assert.DoesNotThrow(() => new EmailAddress(emailAddress));
+            }
+            else
+            {
+                Assert.Throws<DomainException>(() => new EmailAddress(emailAddress));
+            }
+        }
+        
+        
+    }
+}
