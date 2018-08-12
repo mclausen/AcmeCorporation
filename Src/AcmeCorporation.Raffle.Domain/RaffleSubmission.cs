acbcc483b9880557887cd4b1063d0873b7177c51
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 13:59:44 +0200

    - Encapsulated fields on RaffeSubmission - Added fild validation on RaffleSubmission

--- Src/AcmeCorporation.Raffle.Domain/RaffleSubmission.cs
+++ Src/AcmeCorporation.Raffle.Domain/RaffleSubmission.cs
@@ -5,10 +5,26 @@ namespace AcmeCorporation.Raffle.Domain
     public class RaffleSubmission
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public EmailAddress EmailAddress { get; set; }
-        public SerialNumber SerialNumber { get; set; }
-        public DateTime SubmissionTimeUtc { get; set; }
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public EmailAddress EmailAddress { get; protected set; }
+        public SerialNumber SerialNumber { get; protected set; }
+        public DateTime SubmissionTimeUtc { get; protected set; }
+
+
+        public RaffleSubmission(string firstName, string lastName, EmailAddress emailAddress, SerialNumber serialNumber)
+        {
+            if(string.IsNullOrEmpty(firstName)) throw new ArgumentNullException(nameof(firstName));
+            if(string.IsNullOrEmpty(lastName)) throw new ArgumentNullException(nameof(lastName));
+            if(emailAddress == null) throw new ArgumentNullException(nameof(emailAddress));
+            if(serialNumber == null) throw new ArgumentNullException(nameof(serialNumber));
+            
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            SerialNumber = serialNumber;
+            
+            SubmissionTimeUtc = DateTime.UtcNow;
+        }
     }
 }
