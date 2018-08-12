2201d413f113c9a33905133dd67a5a8b978549f7
Martin Humlund Clausen <martinh.c@me.com>
2018-08-12 19:52:23 +0200

    - Shows the draw component on the main page - Updated environment configs to provide endpoints

--- Src/AcmeCorporation-Client/src/environments/environment.ts
+++ Src/AcmeCorporation-Client/src/environments/environment.ts
@@ -3,13 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  endpointUrl: 'https://localhost:5001',
 };
 
 /*
  * In development mode, for easier debugging, you can ignore zone related error
  * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
+ * below file. Don't forget to comment it out in production msode
  * because it will have a performance impact when errors are thrown
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
