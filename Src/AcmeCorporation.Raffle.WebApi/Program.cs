60a3c124bc8a58d0c6798b4c9d0753f88e7de424
Martin Humlund Clausen <martinh.c@me.com>
2018-08-14 01:06:59 +0200

    Only enabled non https port and swapped to Kestrel <3

--- Src/AcmeCorporation.Raffle.WebApi/Program.cs
+++ Src/AcmeCorporation.Raffle.WebApi/Program.cs
@@ -19,6 +19,8 @@ namespace AcmeCorporation.Raffle.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>();
     }
 }
