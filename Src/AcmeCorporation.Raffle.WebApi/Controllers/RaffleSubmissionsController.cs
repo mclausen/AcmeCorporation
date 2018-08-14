24700172d7fc7d0422789043bdafc4c0f7164d1b
Martin Humlund Clausen <martinh.c@me.com>
2018-08-14 10:41:06 +0200

    - Moved dto convertion into extension methods

--- Src/AcmeCorporation.Raffle.WebApi/Controllers/RaffleSubmissionsController.cs
+++ Src/AcmeCorporation.Raffle.WebApi/Controllers/RaffleSubmissionsController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AcmeCorporation.Raffle.Domain;
 using AcmeCorporation.Raffle.Domain.Interfaces;
 using AcmeCorporation.Raffle.Infrastructure.Services;
+using AcmeCorporation.Raffle.WebApi.Extensions;
 using AcmeCorporation.Raffle.WebApi.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,26 +22,13 @@ namespace AcmeCorporation.Raffle.WebApi.Controllers
             _serialNumberRepository = serialNumberRepository;
         }
         
-        [HttpGet, Route("{page}")]
+        [HttpGet, Route("/pages/{page}")] // /pages/to avoid defacto rest getbyId convention
         public async Task<IActionResult> Get(int page)
         {
             var pagedResult = await _submissionService.GetSubmissions(page);
-            var result = new PagedDrawSubmissionsDto()
-            {
-                CurrentPage = pagedResult.CurrentPage,
-                NumberOfPages = pagedResult.NumberOfPages,
-                Submissions = pagedResult.Submissions.Select(x => new DrawSubmissionListingDto()
-                {
-                    Id = x.Id,
-                    EmailAddress = x.EmailAddress.Value,
-                    FirstName = x.FirstName,
-                    LastName = x.LastName,
-                    SerialNumber = x.SerialNumber.Serial,
-                    SubmissionTimeUtc = x.SubmissionTimeUtc
-                }).ToList()
-            };
+            var dto = pagedResult.ToDto();
 
-            return new OkObjectResult(result);
+            return new OkObjectResult(dto);
         }
         
         
@@ -53,15 +41,9 @@ namespace AcmeCorporation.Raffle.WebApi.Controllers
     
             var submission = await _submissionService.Submit(request.FirstName, request.LastName, new EmailAddress(request.EmailAddress),
                 serialNumber);
- 
-            return new OkObjectResult(new DrawSubmissionListingDto()
-            {
-                FirstName = submission.FirstName,
-                LastName = submission.LastName,
-                EmailAddress = submission.EmailAddress.Value,
-                SerialNumber = serialNumber.Serial,
-                SubmissionTimeUtc = submission.SubmissionTimeUtc
-            });
+
+            var dto = submission.ToDto();
+            return new OkObjectResult(dto);
         }
     }
 }
---
+++ Src/AcmeCorporation.Raffle.WebApi/Extensions/DtoExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using AcmeCorporation.Raffle.Domain;
+using AcmeCorporation.Raffle.Domain.Interfaces;
+using AcmeCorporation.Raffle.WebApi.Model;
+
+namespace AcmeCorporation.Raffle.WebApi.Extensions
+{
+    public static class DtoExtensions
+    {
+        public static DrawSubmissionListingDto ToDto(this RaffleSubmission self, bool includeSerial = false)
+        {
+            return new DrawSubmissionListingDto()
+            {
+                Id = self.Id,
+                EmailAddress = self.EmailAddress.Value,
+                FirstName = self.FirstName,
+                LastName = self.LastName,
+                SerialNumber = includeSerial ? self.SerialNumber.Serial : string.Empty,
+                SubmissionTimeUtc = self.SubmissionTimeUtc
+            };
+        }
+
+        public static PagedDrawSubmissionsDto ToDto(this PagedRaffleSubmissionsResult self)
+        {
+            return new PagedDrawSubmissionsDto()
+            {
+                CurrentPage = self.CurrentPage,
+                NumberOfPages = self.NumberOfPages,
+                Submissions = self.Submissions
+                    .Select(draw => draw.ToDto())
+                    .ToList()
+            };
+        }
+    }
+}
