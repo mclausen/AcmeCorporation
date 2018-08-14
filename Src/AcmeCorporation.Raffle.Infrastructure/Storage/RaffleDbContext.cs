c4f10f65725728bd7f840f9b2eb30f54fa7d160b
Martin Humlund Clausen <martinh.c@me.com>
2018-08-14 12:09:22 +0200

    - Removed unused RaffleDbContext -> OnConfiguring method

--- Src/AcmeCorporation.Raffle.Infrastructure/Storage/RaffleDbContext.cs
+++ Src/AcmeCorporation.Raffle.Infrastructure/Storage/RaffleDbContext.cs
@@ -17,9 +17,6 @@ namespace AcmeCorporation.Raffle.Infrastructure.Storage
         
         public RaffleDbContext(DbContextOptions<RaffleDbContext> options) : base(options) {}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(
-            @"");
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // SerialNumber
