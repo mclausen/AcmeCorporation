8b2fb7779fc10daf648c330fb1eb830db2dd359f
Martin Humlund Clausen <martinh.c@me.com>
2018-08-14 11:58:05 +0200

    - Configured startup to not redirect to https (development purposes) - Moved ConnectionString settings to configuration

--- Src/AcmeCorporation.Raffle.WebApi/Startup.cs
+++ Src/AcmeCorporation.Raffle.WebApi/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace AcmeCorporation.Raffle.WebApi
 {
@@ -30,13 +31,12 @@ namespace AcmeCorporation.Raffle.WebApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   
             services
                 .AddMvc(options => { options.Filters.Add<ModelStateValidationFilter>(); })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            
-            var connectionString = @"";
+            var connectionString = Configuration.GetConnectionString("DrawDbConnectionString");
             services
                 .AddDbContext<RaffleDbContext>(contextOptions =>
                 {
@@ -72,8 +72,12 @@ namespace AcmeCorporation.Raffle.WebApi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            //app.UseHttpsRedirection();
+            app.UseMvc(options =>
+            {
+            });
+
+            
 
             using (var dbContext = app.ApplicationServices.GetService<RaffleDbContext>())
             {
