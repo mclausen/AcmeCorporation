a9417d595898abbcab5b7a20bb813c2b4979db53
Martin Humlund Clausen <mc@nabufit.com>
2018-08-16 14:09:00 +0200

    Added Domain Exception handling

---
+++ Src/AcmeCorporation.Draw.WebApi/Filters/DomainErrorFilter.cs
@@ -0,0 +1,25 @@
+﻿using AcmeCorporation.Draw.Domain;
+using AcmeCorporation.Draw.WebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AcmeCorporation.Draw.WebApi.Filters
+{
+    public class DomainErrorFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if(context.Exception != null && context.Exception.GetType() == typeof(DomainException))
+            {
+                var domainException = context.Exception as DomainException;
+                context.Result = new BadRequestObjectResult(new DomainErrorDto()
+                {
+                    IsDomainError = true,
+                    Message = domainException.Message
+                });
+
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
---
+++ Src/AcmeCorporation.Draw.WebApi/Model/DomainErrorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AcmeCorporation.Draw.WebApi.Model
+{
+    public class DomainErrorDto
+    {
+        public bool IsDomainError { get; set; }
+        public string Message { get; set; }
+    }
+}
