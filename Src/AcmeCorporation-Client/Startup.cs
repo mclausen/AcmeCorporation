483b5cd4abbf68ef0a22a86a7be38f588e6ac307
Martin Humlund Clausen <mc@nabufit.com>
2018-08-16 22:37:52 +0200

    Made client work from visual studio!

--- Src/AcmeCorporation-Client/Startup.cs
+++ Src/AcmeCorporation-Client/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -21,6 +22,9 @@ namespace AcmeCorporation.Client
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            ConfigureAngulerRouting(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,6 +34,24 @@ namespace AcmeCorporation.Client
             app.UseMvcWithDefaultRoute();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+          }
+
+        private void ConfigureAngulerRouting(IApplicationBuilder app)
+        {
+            // Angular routing trick, if the url requested is not found we route back to
+            // the index file which will make angular do the routing
+            // But this comes as a cost since it requests the server first instead of just handling the routing directly
+            // For op timal solution, start the client from console with ng server
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                  context.Request.Path = "/index.html";
+                  await next();
+                }
+            });
         }
     }
 }
